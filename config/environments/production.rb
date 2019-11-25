052b065ebe5d1ea7f24712a9a772ee0abf1d854c
Jade Huynh <35jadeh@gmail.com>
2019-11-25 15:52:57 -0600

    asdfs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,8 +102,9 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  # Store files on Amazon S3.
+  config.active_storage.service = :amazon
 end
 
 
-# Store files on Amazon S3.
-config.active_storage.service = :amazon
